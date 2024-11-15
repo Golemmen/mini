a9f3c576fd3587c28a2c00717bc9c359ec75d919
golde <tolegenmurat6@gmail.com>
2024-11-15 17:03:30 +0500

    начало

---
+++ src/main/java/kz/iitu/smart1/Smart1Application.java
@@ -0,0 +1,13 @@
+package kz.iitu.smart1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Smart1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Smart1Application.class, args);
+    }
+
+}
---
+++ src/test/java/kz/iitu/smart1/Smart1ApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.iitu.smart1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Smart1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
